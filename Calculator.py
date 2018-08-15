d39124d2e2f407dc6d4b434a4047fd9b2bff9c9f
adriankruczak <adriankruczak@gmail.com>
2018-08-15 17:00:33 +0200

    Improved calculator

--- Calculator.py
+++ Calculator.py
@@ -1,6 +1,20 @@
-x=int(input("Enter a first value: "))
-y=int(input("Enter a second value: "))
+num1=float(input("Enter a first value: "))
+op = input("Enter an operator: ")
+num2=float(input("Enter a second value: "))
 
-wynik=x+y 
-wynik *=wynik 
-print(wynik)
+
+
+if op == "+":
+    wynik = num1 + num2 
+    print(wynik)
+elif op == "-":
+    wynik = num1 - num2
+    print(wynik)
+elif op == "/":
+    wynik = num1 / num2
+    print(wynik)
+elif op == "*":
+    wynik = num1 * num2
+    print(wynik)
+else:
+    print("Invalid operation")
